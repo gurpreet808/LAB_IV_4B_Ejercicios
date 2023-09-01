77661c6bcc982e492ce9627ea64de14bce52092e
gurpreet808 <gurpreets808@gmail.com>
2023-09-01 13:29:35 -0300

    Agrego ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: "", component: BienvenidoComponent },
+  { path: "bienvenido", component: BienvenidoComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
