426fc2aeb5e0387fb15d910d2dc49c034beaf7a9
gurpreet808 <gurpreets808@gmail.com>
2023-09-01 12:32:54 -0300

    Funcions sumar y promedio de edades funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LAB_IV_4B_Ejercicios';
+
+  edadUno: number | undefined;
+  edadDos: number | undefined;
+
+  suma: number | undefined;
+  promedio: number | undefined;
+
+  promediar(n1: number, n2: number): number {
+    return parseFloat(((n1 + n2) / 2).toFixed(2));
+  }
+
+  sumar(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+
+  calcular(): void {
+    if (this.edadUno != null || this.edadDos != null) {
+      this.promedio = this.promediar(this.edadUno!, this.edadDos!);
+      this.suma = this.sumar(this.edadUno!, this.edadDos!);
+    }
+  }
+
+  limpiar(): void {
+    delete this.edadUno;
+    delete this.edadDos;
+    delete this.promedio;
+    delete this.suma;
+  }
 }
